80c849ae20c4946ad852709b5ca01d8b68339ce7
Prince Jha <156680708+PrinceJha08@users.noreply.github.com>
2025-07-24 17:18:22 +0530

    hello

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
 
 const Header = () => {
-  return <>
-  <nav className='py-6 px-20 flex justify-between items-center'>
-    <Link>
-    <img src='/logo.png' className='h-7'/>
-    </Link>
+    return (
+        <>
+            <nav className='py-6 px-20 flex justify-between items-center'>
+                <Link>
+                    <img src='/logo.png' className='h-7' />
+                </Link>
 
-    <Button variant="outline">Login</Button>
-  </nav>
-  </>
+                <Button variant="outline">Login</Button>
+            </nav>
+        </>
+    )
 }
 
 export default Header
